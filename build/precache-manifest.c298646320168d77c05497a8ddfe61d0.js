cf11d9a1179d467702538c3de03075ebccfa9050
Serkan Akman <serkan.akman@boranka.com>
2020-04-04 17:14:04 +0300

    Env güncellendi-7

---
+++ build/precache-manifest.c298646320168d77c05497a8ddfe61d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9b9e2e6f5038e1b5996ec0174156b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1c84c5068ed2e5aa2c5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d0af05e79c88a7cc68b",
+    "url": "/static/js/2.fd9911a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd9911a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c84c5068ed2e5aa2c5",
+    "url": "/static/js/main.52bd4862.chunk.js"
+  },
+  {
+    "revision": "77cb81bb29f48c39e94a",
+    "url": "/static/js/runtime-main.4e72308c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
